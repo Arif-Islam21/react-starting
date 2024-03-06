5a0093b627f21e2206b534fd7ffd817703d1bd84
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-06 09:31:25 +0600

    working with local storage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const addToCart = () => {
+  const productEl = document.getElementById("product-element");
+  const quantityEl = document.getElementById("quantity-element");
+  const product = productEl.value;
+  const quantity = quantityEl.value;
+  productEl.value = "";
+  quantityEl.value = "";
+  //   console.log(product, quantity);
+  displayLi(product, quantity);
+  saveToLocalStorage(product, quantity);
+};
+
+const displayLi = (product, quantity) => {
+  const liContainer = document.getElementById("li-container");
+  const li = document.createElement("li");
+  li.innerText = `${product} : ${quantity}`;
+  liContainer.appendChild(li);
+};
+
+const saveToLocalStorage = (product, quantity) => {};
