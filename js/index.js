106ca8a731edfdb3e97fb7988a0fbbd371264333
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-06 09:45:49 +0600

    local storage saved

--- js/index.js
+++ js/index.js
@@ -17,4 +17,27 @@ const displayLi = (product, quantity) => {
   liContainer.appendChild(li);
 };
 
-const saveToLocalStorage = (product, quantity) => {};
+const getStoredShoppingCart = () => {
+  const stordeCart = localStorage.getItem("cart");
+  let cart = {};
+  if (stordeCart) {
+    cart = JSON.parse(stordeCart);
+  }
+  return cart;
+};
+
+const saveToLocalStorage = (product, quantity) => {
+  const cart = getStoredShoppingCart();
+  cart[product] = quantity;
+  const cartStringified = JSON.stringify(cart);
+  localStorage.setItem("cart", cartStringified);
+};
+
+const displayFromLocalStorage = () => {
+  const savedCart = getStoredShoppingCart();
+  for (const product in savedCart) {
+    const quantity = savedCart[product];
+    displayLi(product, quantity);
+  }
+};
+displayFromLocalStorage();
